219f5533f5a07355fb38593191f1e92b57b153a3
Siddhant Pawar <79016942+justSid404@users.noreply.github.com>
2024-06-18 01:31:35 +0530

    Implemented logic for changing product image
    - Implemented logic for changing product image when product color or size is selected
    - Added more images for various product variants

--- scripts/home-page.js
+++ scripts/home-page.js
@@ -5,7 +5,7 @@ const cartItemCountElement = document.querySelector('.cartItemCount');
 
 let products = [
   {
-    image : ['images/sockType1.jpg','socks1'],
+    image : ['images/sockType1.jpg'],
     name : 'Black and Gray Athletic Cotton Socks - 6 Pairs',
     reviewStars : 45,
     reviewCount : 87,
@@ -13,7 +13,7 @@ let products = [
     quantity : 1
   },
   {
-    image : ['images/intermediate-composite-basketball.jpg','socks1'],
+    image : ['images/intermediate-composite-basketball.jpg'],
     name : 'Intermediate Size Basketball',
     reviewStars : 40,
     reviewCount : 127,
@@ -21,7 +21,7 @@ let products = [
     quantity : 1
   },
   {
-    image : ['images/adults-plain-cotton-tshirt-2-pack-teal.jpg','socks1'],
+    image : ['images/adults-plain-cotton-tshirt-2-pack-teal.jpg'],
     name : 'Adults Plain Cotton T-Shirt - 2 Pack',
     reviewStars : 45,
     reviewCount : 56,
@@ -29,7 +29,7 @@ let products = [
     quantity : 1
   },
   {
-    image : ['images/black-2-slot-toaster.jpg','socks1'],
+    image : ['images/black-2-slot-toaster.jpg'],
     name : '2 Slot Toaster - Black',
     reviewStars : 50,
     reviewCount : 2197,
@@ -37,7 +37,7 @@ let products = [
     quantity : 1
   },
   {
-    image : ['images/6-piece-white-dinner-plate-set.jpg','socks1'],
+    image : ['images/6-piece-white-dinner-plate-set.jpg'],
     name : '6 Piece White Dinner Plate Set',
     reviewStars : 40,
     reviewCount : 37,
@@ -45,7 +45,7 @@ let products = [
     quantity : 1
   },
   {
-    image : ['images/6-piece-non-stick-baking-set.webp','socks1'],
+    image : ['images/6-piece-non-stick-baking-set.webp'],
     name : '6-Piece Nonstick, Carbon Steel Oven Bakeware Baking Set',
     reviewStars : 45,
     reviewCount : 175,
@@ -53,7 +53,7 @@ let products = [
     quantity : 1
   },
   {
-    image : ['images/plain-hooded-fleece-sweatshirt-yellow.jpg','socks1'],
+    image : ['images/plain-hooded-fleece-sweatshirt-yellow.jpg','images/plain-hooded-fleece-sweatshirt-teal.jpg'],
     name : 'Plain Hooded Fleece Sweatshirt',
     reviewStars : 45,
     reviewCount : 317,
@@ -63,7 +63,7 @@ let products = [
     size : ['S','M','L']
   },
   {
-    image : ['images/luxury-tower-set-6-piece.jpg','socks1'],
+    image : ['images/luxury-tower-set-6-piece.jpg','images/luxury-tower-set-4-piece.jpg'],
     name : 'Luxury Towel Set - Graphite Gray',
     reviewStars : 45,
     reviewCount : 305,
@@ -72,7 +72,7 @@ let products = [
     set : ['6-Piece','4-Piece']
   },
   {
-    image : ['images/liquid-laundry-detergent-plain.jpg','socks1'],
+    image : ['images/liquid-laundry-detergent-plain.jpg','images/liquid-laundry-detergent-lavender.jpg'],
     name : 'Liquid Laundry Detergent, 110 Loads, 82.5 Fl Oz',
     reviewStars : 45,
     reviewCount : 305,
@@ -81,7 +81,7 @@ let products = [
     style : ['Plain','Lavender']
   },
   {
-    image : ['images/knit-athletic-sneakers-gray.jpg','socks1'],
+    image : ['images/knit-athletic-sneakers-gray.jpg'],
     name : 'Waterproof Knit Athletic Sneakers - Gray',
     reviewStars : 40,
     reviewCount : 89,
@@ -90,7 +90,7 @@ let products = [
     size : [5,6,7,8,9]
   },
   {
-    image : ['images/women-chiffon-beachwear-coverup-black.jpg','socks1'],
+    image : ['images/women-chiffon-beachwear-coverup-black.jpg'],
     name : "Women's Chiffon Beachwear Cover Up - Black",
     reviewStars : 45,
     reviewCount : 235,
@@ -98,7 +98,7 @@ let products = [
     quantity : 1
   },
   {
-    image : ['images/round-sunglasses-black.jpg','socks1'],
+    image : ['images/round-sunglasses-black.jpg','images/round-sunglasses-gold.jpg'],
     name : 'Round Sunglasses',
     reviewStars : 45,
     reviewCount : 30,
@@ -107,7 +107,7 @@ let products = [
     style : ['Black','Gold']
   },
   {
-    image : ['images/women-beach-sandals.jpg','socks1'],
+    image : ['images/women-beach-sandals.jpg'],
     name : "Women's Two Strap Buckle Sandals - Tan",
     reviewStars : 45,
     reviewCount : 562,
@@ -116,7 +116,7 @@ let products = [
     size : [7,8,9]
   },
   {
-    image : ['images/blackout-curtain-set-beige.webp','socks1'],
+    image : ['images/blackout-curtain-set-beige.webp'],
     name : 'Blackout Curtains Set 4-Pack - Beige',
     reviewStars : 45,
     reviewCount : 232,
@@ -124,7 +124,7 @@ let products = [
     quantity : 1
   },
   {
-    image : ['images/men-slim-fit-summer-shorts-gray.jpg','socks1'],
+    image : ['images/men-slim-fit-summer-shorts-gray.jpg','images/men-slim-fit-summer-shorts-black.jpg','images/men-slim-fit-summer-shorts-beige.jpg'],
     name : "Men's Slim-Fit Summer Shorts",
     reviewStars : 40,
     reviewCount : 160,
@@ -134,7 +134,7 @@ let products = [
     size : [30,31,32]
   },
   {
-    image : ['images/electric-glass-and-steel-hot-water-kettle.webp','socks1'],
+    image : ['images/electric-glass-and-steel-hot-water-kettle.webp'],
     name : 'Electric Glass and Steel Hot Tea Water Kettle - 1.7-Liter',
     reviewStars : 50,
     reviewCount : 846,
@@ -142,7 +142,7 @@ let products = [
     quantity : 1
   },
   {
-    image : ['images/facial-tissue-2-ply-18-boxes.jpg','socks1'],
+    image : ['images/facial-tissue-2-ply-18-boxes.jpg'],
     name : 'Ultra Soft Tissue 2-Ply - 18 Box',
     reviewStars : 40,
     reviewCount : 99,
@@ -150,7 +150,7 @@ let products = [
     quantity : 1
   },
   {
-    image : ['images/straw-sunhat.webp','socks1'],
+    image : ['images/straw-sunhat.webp'],
     name : 'Straw Lifeguard Sun Hat',
     reviewStars : 40,
     reviewCount : 215,
@@ -158,7 +158,7 @@ let products = [
     quantity : 1
   },
   {
-    image : ['images/sky-flower-stud-earrings.webp','socks1'],
+    image : ['images/sky-flower-stud-earrings.webp'],
     name : 'Sterling Silver Sky Flower Stud Earrings',
     reviewStars : 45,
     reviewCount : 52,
@@ -166,7 +166,7 @@ let products = [
     quantity : 1
   },
   {
-    image : ['images/women-stretch-popover-hoodie-black.jpg','socks1'],
+    image : ['images/women-stretch-popover-hoodie-black.jpg','images/women-stretch-popover-hoodie-gray.jpg','images/women-stretch-popover-hoodie-blue.jpg'],
     name : "Women's Stretch Popover Hoodie",
     reviewStars : 45,
     reviewCount : 2465,
@@ -176,7 +176,7 @@ let products = [
     size : ['XS','S','M','L']
   },
   {
-    image : ['images/bathroom-rug.jpg','socks1'],
+    image : ['images/bathroom-rug.jpg'],
     name : 'Bathroom Bath Rug Mat 20 x 31 Inch - Grey',
     reviewStars : 45,
     reviewCount : 119,
@@ -184,7 +184,7 @@ let products = [
     quantity : 1
   },
   {
-    image : ['images/women-knit-ballet-flat-black.jpg','socks1'],
+    image : ['images/women-knit-ballet-flat-black.jpg','images/women-knit-ballet-flat-gray.jpg'],
     name : "Women's Knit Ballet Flat",
     reviewStars : 40,
     reviewCount : 326,
@@ -194,7 +194,7 @@ let products = [
     size : [6,7,8]
   },
   {
-    image : ['images/men-golf-polo-t-shirt-blue.jpg','socks1'],
+    image : ['images/men-golf-polo-t-shirt-blue.jpg','images/men-golf-polo-t-shirt-black.jpg','images/men-golf-polo-t-shirt-red.jpg'],
     name : "Men's Regular-Fit Quick-Dry Golf Polo Shirt",
     reviewStars : 45,
     reviewCount : 2556,
@@ -204,7 +204,7 @@ let products = [
     size : ['S','M','L']
   },
   {
-    image : ['images/trash-can-with-foot-pedal-50-liter.jpg','socks1'],
+    image : ['images/trash-can-with-foot-pedal-50-liter.jpg','images/trash-can-with-foot-pedal-30-liter-tall.jpg'],
     name : 'Trash Can with Foot Pedal - Brushed Stainless Steel',
     reviewStars : 45,
     reviewCount : 2286,
@@ -213,7 +213,7 @@ let products = [
     size : ['50L', '30L Tall']
   },
   {
-    image : ['images/duvet-cover-set-blue-twin.jpg','socks1'],
+    image : ['images/duvet-cover-set-blue-twin.jpg','images/duvet-cover-set-blue-queen.jpg','images/duvet-cover-set-red-twin.jpg','images/duvet-cover-set-red-queen.jpg'],
     name : 'Duvet Cover Set with Zipper Closure',
     reviewStars : 40,
     reviewCount : 456,
@@ -223,7 +223,7 @@ let products = [
     size : ['Twin', 'Queen']
   },
   {
-    image : ['images/women-chunky-beanie-gray.webp','socks1'],
+    image : ['images/women-chunky-beanie-gray.webp'],
     name : "Women's Chunky Cable Beanie - Gray",
     reviewStars : 50,
     reviewCount : 83,
@@ -231,7 +231,7 @@ let products = [
     quantity : 1
   },
   {
-    image : ['images/men-chino-pants-beige.jpg','socks1'],
+    image : ['images/men-chino-pants-beige.jpg','images/men-chino-pants-green.jpg','images/men-chino-pants-black.jpg'],
     name : "Men's Classic-fit Pleated Chino Pants",
     reviewStars : 45,
     reviewCount : 9017,
@@ -241,7 +241,7 @@ let products = [
     size : [30,31,32]
   },
   {
-    image : ['images/men-athletic-shoes-green.jpg','socks1'],
+    image : ['images/men-athletic-shoes-green.jpg','images/men-athletic-shoes-black.jpg'],
     name : "Men's Athletic Sneaker",
     reviewStars : 40,
     reviewCount : 229,
@@ -251,7 +251,7 @@ let products = [
     size : [9,10,11,12]
   },
   {
-    image : ['images/men-navigator-sunglasses-brown.jpg','socks1'],
+    image : ['images/men-navigator-sunglasses-brown.jpg','images/men-navigator-sunglasses-silver.jpg'],
     name : "Men's Navigator Sunglasses Pilot",
     reviewStars : 35,
     reviewCount : 42,
@@ -260,7 +260,7 @@ let products = [
     color : ['Brown','Silver']
   },
   {
-    image : ['images/non-stick-cooking-set-15-pieces.webp','socks1'],
+    image : ['images/non-stick-cooking-set-15-pieces.webp'],
     name : 'Non-Stick Cookware Set, Pots, Pans and Utensils - 15 Pieces',
     reviewStars : 45,
     reviewCount : 511,
@@ -268,7 +268,7 @@ let products = [
     quantity : 1
   },
   {
-    image : ['images/vanity-mirror-silver.jpg','socks1'],
+    image : ['images/vanity-mirror-silver.jpg'],
     name : 'Vanity Mirror with Heavy Base - Chrome',
     reviewStars : 45,
     reviewCount : 130,
@@ -276,7 +276,7 @@ let products = [
     quantity : 1
   },
   {
-    image : ['images/women-french-terry-fleece-jogger-camo.jpg','socks1'],
+    image : ['images/women-french-terry-fleece-jogger-camo.jpg','images/women-french-terry-fleece-jogger-gray.jpg'],
     name : "Women's Fleece Jogger Sweatpant",
     reviewStars : 45,
     reviewCount : 248,
@@ -286,7 +286,7 @@ let products = [
     size : ['S','M','L']
   },
   {
-    image : ['images/double-elongated-twist-french-wire-earrings.webp','socks1'],
+    image : ['images/double-elongated-twist-french-wire-earrings.webp'],
     name : 'Double Oval Twist French Wire Earrings - Gold',
     reviewStars : 45,
     reviewCount : 117,
@@ -294,7 +294,7 @@ let products = [
     quantity : 1
   },
   {
-    image : ['images/round-airtight-food-storage-containers.jpg','socks1'],
+    image : ['images/round-airtight-food-storage-containers.jpg'],
     name : 'Round Airtight Food Storage Containers - 5 Piece',
     reviewStars : 40,
     reviewCount : 126,
@@ -302,7 +302,7 @@ let products = [
     quantity : 1
   },
   {
-    image : ['images/coffeemaker-with-glass-carafe-black.jpg','socks1'],
+    image : ['images/coffeemaker-with-glass-carafe-black.jpg'],
     name : 'Coffeemaker with Glass Carafe and Reusable Filter - 25 Oz,...',
     reviewStars : 45,
     reviewCount : 1211,
@@ -310,7 +310,7 @@ let products = [
     quantity : 1
   },
   {
-    image : ['images/blackout-curtains-black.jpg','socks1'],
+    image : ['images/blackout-curtains-black.jpg'],
     name : 'Blackout Curtains Set 42 x 84-Inch - Black, 2 Panels',
     reviewStars : 45,
     reviewCount : 363,
@@ -318,7 +318,7 @@ let products = [
     quantity : 1
   },
   {
-    image : ['images/cotton-bath-towels-teal.webp','socks1'],
+    image : ['images/cotton-bath-towels-teal.webp'],
     name : '100% Cotton Bath Towels - 2 Pack, Light Teal',
     reviewStars : 45,
     reviewCount : 93,
@@ -326,7 +326,7 @@ let products = [
     quantity : 1
   },
   {
-    image : ['images/knit-athletic-sneakers-pink.webp','socks1'],
+    image : ['images/knit-athletic-sneakers-pink.webp'],
     name : 'Waterproof Knit Athletic Sneakers - Pink',
     reviewStars : 40,
     reviewCount : 89,
@@ -335,7 +335,7 @@ let products = [
     size : [6,7,8,9]
   },
   {
-    image : ['images/countertop-blender-64-oz.jpg','socks1'],
+    image : ['images/countertop-blender-64-oz.jpg'],
     name : 'Countertop Blender - 64oz, 1400 Watts',
     reviewStars : 40,
     reviewCount : 3,
@@ -343,7 +343,7 @@ let products = [
     quantity : 1
   },
   {
-    image : ['images/floral-mixing-bowl-set.jpg','socks1'],
+    image : ['images/floral-mixing-bowl-set.jpg'],
     name : '10-Piece Mixing Bowl Set with Lids - Floral',
     reviewStars : 50,
     reviewCount : 679,
@@ -351,7 +351,7 @@ let products = [
     quantity : 1
   },
   {
-    image : ['images/kitchen-paper-towels-30-pack.jpg','socks1'],
+    image : ['images/kitchen-paper-towels-30-pack.jpg'],
     name : '2-Ply Kitchen Paper Towels - 30 Pack',
     reviewStars : 45,
     reviewCount : 1045,
@@ -359,7 +359,7 @@ let products = [
     quantity : 1
   },
   {
-    image : ['images/men-cozy-fleece-zip-up-hoodie-red.jpg','socks1'],
+    image : ['images/men-cozy-fleece-zip-up-hoodie-red.jpg','images/men-cozy-fleece-zip-up-hoodie-black.jpg'],
     name : "Men's Full-Zip Hooded Fleece Sweatshirt",
     reviewStars : 45,
     reviewCount : 3157,
@@ -428,6 +428,8 @@ function showResult(products) {
         tempColorBtnElements.addEventListener('click', () => {
           
           unselectAllAndSelectOne('color', tempColorBtnElements, index);
+
+          updateProductImage(product, index);
           
         });
       });
@@ -447,6 +449,8 @@ function showResult(products) {
         tempSizeBtnElements.addEventListener('click', () => {
           
           unselectAllAndSelectOne('size', tempSizeBtnElements, index);
+
+          updateProductImage(product, index);
           
         });
       });
@@ -527,6 +531,8 @@ function showResult(products) {
       }
   
     });
+
+    updateProductImage(product, index);
   
   });
 
@@ -542,7 +548,7 @@ function addProductHTML(product, index) {
 
     <div class="product-img-div">
 
-      <img class = "product-img" src="${product.image[0]}" alt="${product.image[1]}">
+      <img class = "product-img" src="${product.image[0]}">
 
     </div>
 
@@ -759,6 +765,128 @@ function unselectAllAndSelectOne(variantType, btnElement, productIndex) {
   if(btnElement.classList.contains('button-accent')) {
     btnElement.classList.remove('button-accent');
     btnElement.classList.add('button-clicked-accent');
+  }
+
+}
+
+function updateProductImage(product, index) {
+  
+  let colorVariantClasses = document.querySelectorAll(`[class*="product-${index}-color-variant"]`);
+  let selectedColor;
+  
+  colorVariantClasses.forEach((value) => {
+    if(value.classList.contains('button-clicked-accent')) {
+      if(value.value !== undefined) {
+        selectedColor = value.value.toLowerCase();
+      }
+    }
+  });
+  
+  let sizeVariantClasses = document.querySelectorAll(`[class*="product-${index}-size-variant"]`);
+
+  let selectedSize;
+  
+  sizeVariantClasses.forEach((value) => {
+    if(value.classList.contains('button-clicked-accent')) {
+      if(value.value !== undefined) {
+        selectedSize = value.value.toLowerCase();
+      }
+    }
+  });
+
+  if(selectedSize === 's') {
+    selectedSize = 'small';
+  } else if(selectedSize === 'm') {
+    selectedSize = 'medium';
+  } else if(selectedSize === 'l') {
+    selectedSize = 'large';
+  } else if(selectedSize === 'xs') {
+    selectedSize = 'extra-small';
+  }
+
+  console.log('Size: '+selectedSize);
+  console.log('Color: '+selectedColor);
+
+  let productImages = [];
+
+
+  if(selectedSize !== undefined && selectedColor !== undefined) {
+
+    //Size and Color exist
+    //Change required
+
+    product.image.forEach((imagePath) => {
+
+      if(imagePath.includes(selectedColor)) {
+        productImages.push(imagePath);
+      }
+
+    });
+
+    console.log('Product Images -> '+productImages);
+
+    productImages.forEach((imgPth) => {
+
+      if(imgPth.includes(selectedSize)) {
+
+        document.querySelector(`.product-${index}`).getElementsByClassName('product-img')[0].src = imgPth;
+
+      } else {
+
+        document.querySelector(`.product-${index}`).getElementsByClassName('product-img')[0].src = productImages[0];
+
+      }
+
+    });
+
+  }
+
+  else if(selectedSize === undefined && selectedColor !== undefined) {
+
+    //No Size but Color exists
+    //Change required
+
+    product.image.forEach((imagePath) => {
+
+      if(imagePath.includes(selectedColor)) {
+        productImages.push(imagePath);
+      }
+
+    });
+
+    console.log('Product Images -> '+productImages);
+    document.querySelector(`.product-${index}`).getElementsByClassName('product-img')[0].src = productImages[0];
+
+  }
+
+  else if(selectedSize !== undefined && selectedColor === undefined) {
+
+    //No Color but Size exists
+    //Change required
+
+    product.image.forEach((imagePath) => {
+
+      if(imagePath.includes(selectedSize)) {
+        productImages.push(imagePath);
+      }
+
+    });
+
+    console.log('Product Images -> '+productImages);
+
+    if(productImages.length > 0) {
+
+      document.querySelector(`.product-${index}`).getElementsByClassName('product-img')[0].src = productImages[0];
+
+    }
+
+  }
+
+  else if(selectedSize === undefined && selectedColor === undefined) {
+
+    //No Size and No Color
+    //No change required
+
   }
 
 }
